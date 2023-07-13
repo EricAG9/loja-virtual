f8604571beaae89d5523586bea37488d6e262eec
Eric Galvão <ericagoficial@gmail.com>
2023-07-12 22:53:11 -0300

    criado todas as cruds

--- backend/src/main/java/com/dev/backend/entity/Produto.java
+++ backend/src/main/java/com/dev/backend/entity/Produto.java
@@ -5,28 +5,34 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Table;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import lombok.Data;
 
 @Entity
-@Table(name = "cidade")
+@Table(name = "produto")
 @Data
-public class Cidade {
+public class Produto {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
-    private String nome;
+
+    private String descricaoCurta;
+    private String descricaoDetalhada;
+    private Double valorCusto;
+    private Double valorVenda;
     @ManyToOne
-    @JoinColumn(name="idEstado")
-    private Estado estado;
+    @JoinColumn(name="idMarca")
+    private Marca marca;
+    @ManyToOne
+    @JoinColumn(name="idCategoria")
+    private Categoria categoria;
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataCriacao;
     @Temporal(TemporalType.TIMESTAMP)
